b8e6d9e9fcda5adac3f4180f2c952b24ec47827e
Elian Ibaj <elianibaj@gmail.com>
2020-12-31 16:56:10 +0100

    add main functionality

--- content_script.js
+++ content_script.js
@@ -1,5 +1,102 @@
+let selectedNodes = [document.body];
+let results = [];
+let index = -1;
+
+function selectNodes(selector) {
+  selector = selector.trim();
+  if (!selector || selector === "*") {
+    selectedNodes = [document.body];
+    return;
+  }
+
+  if (selector.endsWith("*")) selector = selector.slice(0, -1).trim();
+
+  let arr1 = Array.from(document.body.querySelectorAll(selector));
+  let arr2 = Array.from(document.body.querySelectorAll(`${selector} *`));
+
+  selectedNodes = arr1.filter((x) => !arr2.includes(x));
+}
+
+function search(query) {
+  results = [];
+  index = -1;
+  highlight(null);
+
+  if (!query) return;
+
+  selectedNodes.forEach((el, i) => {
+    walk_the_DOM(el, (node) => {
+      if (node.nodeType === Node.TEXT_NODE && Boolean(node.textContent)) {
+        const regex = RegExp(query.toLowerCase(), "g");
+        const nodeText = node.textContent.toLowerCase();
+        while (regex.test(nodeText)) {
+          results.push({
+            node,
+            start: regex.lastIndex - query.length,
+            end: regex.lastIndex,
+          });
+        }
+      }
+    });
+  });
+
+  next();
+  // TODO in popup: document.getElementById("total").textContent = results.length;
+}
+
+function highlight(current) {
+  // TODO in popup: document.getElementById("index").textContent = index + 1;
+
+  if (!current) return;
+  current.node.parentElement.scrollIntoViewIfNeeded();
+  let selection = document.getSelection();
+  selection.setBaseAndExtent(
+    current.node,
+    current.start,
+    current.node,
+    current.end
+  );
+}
+
+function prev() {
+  if (!results.length) return;
+
+  if (index === 0) index = results.length;
+  highlight(results[--index]);
+}
+
+function next() {
+  if (!results.length) return;
+
+  if (index === results.length - 1) index = -1;
+  highlight(results[++index]);
+}
+
+var walk_the_DOM = function walk(node, func) {
+  func(node);
+  node = node.firstChild;
+  while (node) {
+    walk(node, func);
+    node = node.nextSibling;
+  }
+};
+
 chrome.runtime.onConnect.addListener(function (port) {
   port.onMessage.addListener(function (msg) {
-    console.log(msg.text);
+    switch (msg.type) {
+      case "PREV":
+        prev();
+        break;
+      case "NEXT":
+        next();
+        break;
+      case "SELECT_AND_SEARCH":
+        selectNodes(msg.selector);
+        search(msg.query);
+        break;
+      case "SEARCH":
+        search(msg.query);
+        break;
+    }
   });
 });
--- popup.js
+++ popup.js
@@ -1,7 +1,43 @@
+const btnPrev = document.getElementById("prev");
+const btnNext = document.getElementById("next");
+const selectorEl = document.getElementById("selector");
+const queryEl = document.getElementById("query");
+
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   const port = chrome.tabs.connect(tabs[0].id);
 
-  document.getElementById("selector").addEventListener("input", function (e) {
-    port.postMessage({ text: e.currentTarget.value });
+  btnPrev.addEventListener("click", () => {
+    port.postMessage({ type: "PREV" });
   });
+
+  btnNext.addEventListener("click", () => {
+    port.postMessage({ type: "NEXT" });
+  });
+
+  selectorEl.addEventListener("input", () => {
+    port.postMessage({
+      type: "SELECT_AND_SEARCH",
+      selector: selectorEl.value,
+      query: queryEl.value,
+    });
+  });
+
+  queryEl.addEventListener("input", () => {
+    port.postMessage({ type: "SEARCH", query: queryEl.value });
+  });
+
+  selectorEl.addEventListener("keyup", (e) => {
+    handleEnter(e);
+  });
+  queryEl.addEventListener("keyup", (e) => {
+    handleEnter(e);
+  });
+
+  function handleEnter(e) {
+    if (e.keyCode === 13 && e.shiftKey) {
+      port.postMessage({ type: "PREV" });
+    } else if (e.keyCode === 13) {
+      port.postMessage({ type: "NEXT" });
+    }
+  }
 });
